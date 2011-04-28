8b251b326eea0915419158821646db141219c717
Shane Riley <shaner@hashrocket.com>
2011-04-28 07:17:54 -0400

    Updated to version 1.1 Add options hash with ability to clear onsubmit, change html for notice dialog, add notice selector for pre-existing notice html, namespace all events.

--- jquery.remember-state.js
+++ jquery.remember-state.js
@@ -1,6 +1,7 @@
 (function($) {
   /* jQuery form remember state plugin
      Name: rememberState
+     Version: 1.1
      Description: When called on a form element, localStorage is used to
      remember the values that have been input up to the point of either
      saving or unloading. (closing window, navigating away, etc.) If
@@ -10,9 +11,38 @@
      on the anchor within the remember_state container. If the element doesn't
      exist, it is created and prepended to the form.
      Usage: $("form").rememberState("my_object_name");
+     Notes: To trigger the deletion of a form's localStorage object from
+     outside the plugin, trigger the reset_state event on the form element
+     by using $("form").trigger("reset_state");
   */
-  $.fn.rememberState = function(obj_name) {
-    var use_ids = !(!!obj_name);
+  $.fn.rememberState = function(defaults) {
+    var opts = $.extend({
+          clearOnSubmit: true,
+          noticeDialog: (function() {
+            var $e = $("<p />", {"class": "remember_state"})
+                     .html('Do you want to <a href="#">restore your previously entered info</a>?');
+            return $e;
+          })(),
+          noticeSelector: ".remember_state",
+          objName: false }, defaults);
+    var use_ids = !(!!opts.objName);
+    if (opts.noticeDialog.length && typeof opts.noticeDialog === "object") {
+      opts.noticeDialog.find("a").bind("click.remember_state", function() {
+        var data = localStorage.getObject(opts.objName),
+            $f = $(this).closest("form"),
+            $e;
+        for (var i in data) {
+          $e = $f.find("[name=" + data[i].name + "]");
+          if ($e.is(":radio")) {
+            $e.filter("[value=" + data[i].value + "]").attr("checked", true);
+          }
+          else if ($e.is(":checkbox")) { $e.attr("checked", true); }
+          else { $e.val(data[i].value); }
+        }
+        opts.noticeDialog.remove();
+        return false;
+      });
+    }
     if (this.length > 1) {
       if (console.log) {
         console.log("WARNING: Cannot process more than one form with the same" +
@@ -24,7 +54,7 @@
       var $form = $(this);
       if (use_ids) {
         if ($form.attr("id")) {
-          obj_name = $form.attr("id");
+          opts.objName = $form.attr("id");
         }
         else {
           if (console.log) {
@@ -34,32 +64,24 @@
           }
         }
       }
-      if (localStorage[obj_name]) {
-        if (!$form.find(".remember_state").length) {
-          $("<p />", {"class": "remember_state"})
-            .html('Do you want to <a href="#">restore your previously entered info</a>?')
-            .prependTo($form);
-        }
-        else { $(".remember_state").show(); }
-        $(".remember_state a").click(function() {
-          var personal_info = localStorage.getObject(obj_name);
-          for (var i in personal_info) {
-            var $e = $form.find("[name=" + personal_info[i].name + "]");
-            if ($e.is(":radio")) {
-              $e.filter("[value=" + personal_info[i].value + "]").attr("checked", true);
-            }
-            else if ($e.is(":checkbox")) { $e.attr("checked", true); }
-            else { $e.val(personal_info[i].value); }
-          }
-          $(this).closest(".remember_state").remove();
-          return false;
+      if (localStorage[opts.objName]) {
+        (opts.noticeDialog.length && typeof opts.noticeDialog === "object") ?
+          opts.noticeDialog.prependTo($form) :
+          $form.find(opts.noticeSelector).show();
+      }
+      $form.bind("reset_state", function() {
+        delete localStorage[opts.objName];
+      });
+      if (opts.clearOnSubmit) {
+        $form.bind("submit.remember_state", function() {
+          $(this).trigger("reset_state");
         });
       }
-      $(window).unload(function() {
-        var personal_info = localStorage.setObject(obj_name, $("form").serializeArray());
+      $(window).bind("unload.remember_state", function() {
+        var personal_info = localStorage.setObject(opts.objName, $("form").serializeArray());
       });
-      $("form").find(":reset").click(function() {
-        delete localStorage[obj_name];
+      $form.find(":reset").bind("click.remember_state", function() {
+        $(this).closest("form").trigger("reset_state");
       });
     });
   };
--- tests.js
+++ tests.js
@@ -1,4 +1,6 @@
-var o = "remember_state_test_data";
+var o = {
+  objName: "remember_state_test_data"
+};
 var setup = function() {
   return $("#qunit-fixture").find("form").rememberState(o);
 };
@@ -14,37 +16,37 @@ test("Requirements", 3, function() {
 
 module("rememberState", {
   setup: function() {
-    delete localStorage[o];
+    delete localStorage[o.objName];
   }
 });
 
 test("It should have no data in localStorage", function() {
-  ok(!localStorage[o]);
+  ok(!localStorage[o.objName]);
 });
 
 test("Value in text field should save state", function() {
   var $form = setup();
   $form.find("#first_name").val("Shane");
   triggerUnload();
-  ok(/Shane/.test(localStorage.getObject(o)[0].value), "First name saved");
-  $form.find("#lastt_name").val("Riley");
-  ok(!(/Riley/.test(localStorage.getObject(o)[0].value)), "Last name not saved");
+  ok(/Shane/.test(localStorage.getObject(o.objName)[0].value), "First name saved");
+  $form.find("#last_name").val("Riley");
+  ok(!(/Riley/.test(localStorage.getObject(o.objName)[0].value)), "Last name not saved");
 });
 
 test("Value in radio should save state", function() {
   var $form = setup();
   $form.find("#gender_male").attr("checked", true);
   triggerUnload();
-  ok(/Male/.test(localStorage[o]), "Gender saved");
+  ok(/Male/.test(localStorage[o.objName]), "Gender saved");
   $form.find("#gender_female").attr("checked", true);
-  ok(!(/Female/.test(localStorage[o])), "Gender not saved");
+  ok(!(/Female/.test(localStorage[o.objName])), "Gender not saved");
 });
 
 test("Value in select box should save state", function() {
   var $form = setup();
   $form.find("#marital_status option:contains(Married)").attr("selected", true);
   triggerUnload();
-  ok(/Married/.test(localStorage[o]), "Marital status saved");
+  ok(/Married/.test(localStorage[o.objName]), "Marital status saved");
   $form.find("#marital_status option:contains(Single)").attr("selected", true);
   ok(!(/Single/.test(localStorage[0])), "Marital status not saved");
 });
@@ -53,7 +55,7 @@ test ("Value in checkbox should save state", function() {
   var $form = setup();
   $form.find("[name=video_games]").attr("checked", true);
   triggerUnload();
-  ok(/Video/.test(localStorage[o]), "Video games saved");
+  ok(/Video/.test(localStorage[o.objName]), "Video games saved");
   $form.find("[name=dendrophilia]").attr("checked", true);
-  ok(!(/Dendro/.test(localStorage[o])), "Dendrophilia not saved");
+  ok(!(/Dendro/.test(localStorage[o.objName])), "Dendrophilia not saved");
 });
