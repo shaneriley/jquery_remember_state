834239eecb8bd94caa0a2383d08098d251a67141
Constantin Chuprik <constantinchuprik@gmail.com>
2013-09-04 18:41:00 +0700

    Empty array opts.

--- jquery.remember-state.js
+++ jquery.remember-state.js
@@ -169,7 +169,7 @@
     createPlugin: function(plugin) {
       $.fn[plugin.name] = function(opts) {
         var $els = this,
-            method = $.isPlainObject(opts) || !opts ? "" : opts,
+            method = $.isPlainObject(opts) || $.isEmptyObject(opts) ? "" : opts,
             args = arguments;
         if (method && plugin[method]) {
           $els.each(function(i) {
