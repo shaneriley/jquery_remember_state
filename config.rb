834cbcf4842f0a77b375e56eb8349baebf4cb491
Shane Riley <shane@shaneriley.com>
2013-09-28 04:10:15 -0400

    Setup Middleman build config

--- config.rb
+++ config.rb
@@ -56,7 +56,7 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
   # activate :minify_javascript
@@ -65,7 +65,7 @@ configure :build do
   # activate :cache_buster
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Compress PNGs after build
   # First: gem install middleman-smusher
